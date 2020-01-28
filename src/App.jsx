d6def5490f09d4989ab1261fcda0c831904b7859
Brandon Paris <brandon@voxel51.com>
2020-01-27 22:24:36 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import TimerInput from './TimerInput';
+import TimeDisplay from './TimeDisplay';
+
+const CenteredTimer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const AlertText = styled.p`
+  color: hsl(216, 10%, 10%);
+  font-weight: 500;
+  font-size: 2rem;
+  margin-bottom: 0;
+  margin-top: 2rem;
+`;
+
+const App = () => {
+  const [secondsLeft, setSecondsLeft] = useState(0);
+  const [minutes, setMinutes] = useState('0');
+  const [rate, setRate] = useState(1000);
+  const [paused, setPaused] = useState(false);
+  const [isCounting, setIsCounting] = useState(false);
+
+  const halfOriginal = (minutes / 2) * 60;
+
+  let countdownInterval;
+
+  useEffect(() => {
+    if (secondsLeft) {
+      countdownInterval = setInterval(() => {
+        setSecondsLeft(secondsLeft - 1);
+      }, rate);
+    }
+    return () => {
+      clearInterval(countdownInterval);
+    };
+  }, [secondsLeft]);
+
+  const getSeconds = () => (`0${secondsLeft % 60}`).slice(-2);
+  const getMinutes = () => (Math.floor(secondsLeft / 60).toString());
+
+  const handlePause = () => {
+    setPaused(true);
+    clearInterval(countdownInterval);
+  };
+
+  const handleResume = () => {
+    setPaused(false);
+    setSecondsLeft(secondsLeft - 1);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (isCounting) {
+      setPaused(false);
+      setSecondsLeft(0);
+      setIsCounting(false);
+      setMinutes(0);
+    } else {
+      setPaused(false);
+      setIsCounting(true);
+      const minutesToSeconds = minutes * 60;
+      setSecondsLeft(minutesToSeconds - 1);
+    }
+  };
+
+  const handleChange = e => {
+    setMinutes(e.target.value);
+    setIsCounting(false);
+  };
+
+  return (
+    <CenteredTimer>
+      <TimerInput
+        isCounting={isCounting}
+        minutes={minutes}
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+      />
+      {
+        secondsLeft <= halfOriginal
+          && isCounting
+          && secondsLeft >= 1 ? <AlertText>More than halfway there!</AlertText> : null
+      }
+      {
+        secondsLeft === 0 && isCounting ? <AlertText>Time is up!</AlertText> : null
+      }
+      <TimeDisplay
+        seconds={getSeconds()}
+        minutes={getMinutes()}
+        pause={handlePause}
+        paused={paused}
+        resume={handleResume}
+        setRate={setRate}
+        rate={rate}
+      />
+    </CenteredTimer>
+  );
+};
+
+export default App;
---
+++ src/TimerInput.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button, FormControl, InputLabel, Input,
+} from '@material-ui/core';
+import styled from 'styled-components';
+
+const StyledForm = styled.form`
+  display: flex;
+  align-items: center;
+  margin-top: 5rem;
+`;
+
+const TimerInput = ({
+  minutes, handleChange, handleSubmit, isCounting,
+}) => (
+  <StyledForm onSubmit={handleSubmit}>
+    <FormControl>
+      <InputLabel htmlFor="timer">Countdown (minutes)</InputLabel>
+      <Input
+        placeholder="5"
+        onChange={handleChange}
+        id="timer"
+        inputProps={{ min: '0' }}
+        name="timer"
+        type="number"
+        value={minutes}
+        required
+      />
+    </FormControl>
+    <Button disabled={minutes === '0'} style={{ marginLeft: '1rem' }} color="primary" variant="contained" type="submit">
+      {isCounting ? 'Clear' : 'Submit'}
+    </Button>
+  </StyledForm>
+);
+
+TimerInput.propTypes = {
+  minutes: PropTypes.string,
+  handleChange: PropTypes.func,
+  handleSubmit: PropTypes.func,
+  isCounting: PropTypes.bool,
+};
+
+TimerInput.defaultProps = {
+  minutes: '0',
+  handleChange: () => {},
+  handleSubmit: () => {},
+  isCounting: false,
+};
+
+export default TimerInput;
